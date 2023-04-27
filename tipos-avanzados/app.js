c7dad2b70402068a467ec819156aef06d572a863
imcasero <diegocaserosmr@gmail.com>
2023-04-27 16:40:39 +0200

    tipos avanzados actualizados

--- tipos-avanzados/app.js
+++ tipos-avanzados/app.js
@@ -7,3 +7,24 @@ var person2 = {
     name: "Doe",
     age: 12
 };
+var personiner = {
+    name: 'Juan',
+    sayHello: function () { }
+};
+var personiner2 = {
+    name: 'Juan',
+    age: 14,
+    sayHello: function () { }
+};
+//Objetos
+//Para crear un objeto tenemos que tiparlo como interface o como type
+//Records
+//Para objetos dinamicos
+var myObjet = {};
+//a partir de aqui se rellena solo
+myObjet.name = 'pep';
+myObjet.age = 29;
+var persona = {
+    name: 'Jonh',
+    age: 12
+};
--- tipos-avanzados/app.ts
+++ tipos-avanzados/app.ts
@@ -16,3 +16,40 @@ const person2: Person = {
   name: "Doe",
   age: 12
 };
+//interface mas para objetos
+interface Personiner {
+  name:String,
+  age?:number,
+  sayHello():void //asi podemos definir metodos
+}
+const personiner: Personiner = {
+  name: 'Juan',
+  sayHello: function(){}
+};
+const personiner2: Personiner = {
+  name: 'Juan',
+  age: 14,
+  sayHello: function(){}
+};
+//Objetos
+//Para crear un objeto tenemos que tiparlo como interface o como type
+
+//Records
+//Para objetos dinamicos
+const myObjet:Record<string , any> = {};
+
+//a partir de aqui se rellena solo
+myObjet.name = 'pep';
+myObjet.age = 29;
+//Union
+type PersonName ={
+  name:string;
+}
+type PersonAge = {
+  age:number;
+}
+type personUnion = PersonName & PersonAge;
+const persona: personUnion = {
+  name: 'Jonh',
+  age: 12
+}
